031fa7de6e7dc38aeafb6e29b63d2d5edea47442
launchpad5682 <launchpad5682@gmail.com>
2021-07-23 12:49:47 +0530

    App updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
+
+const business = [
+  { book: "Never Split the Difference", rating: "3.5/5" },
+  { book: "Loonshots", rating: "5/5" },
+];
+
+const fiction = [
+  { book: "Shiva Triology", rating: "5/5" },
+  { book: "Harry Potter and the Saucer's Stone", rating: "4.5/5" },
+];
+
+const javascript = [
+  {
+    book: "Eloquent Javascript",
+    rating: "4/5",
+  },
+  {
+    book: "You don't know JS",
+    rating: "3.5/5",
+  },
+];
 
 function App() {
+  const [list, setList] = useState(javascript);
+
+  function clickHandler(event) {
+    // console.log(event.target.value);
+    let buttonLabel = event.target.value;
+    if (buttonLabel === "javascript") {
+      setList(javascript);
+    } else if (buttonLabel === "fiction") {
+      setList(fiction);
+    } else {
+      setList(business);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>📖 Good Books</h1>
+      <h4>Checkout my favorite books. Select a genre to get started</h4>
+      <nav>
+        <button onClick={clickHandler} value="javascript">
+          Javascript
+        </button>
+        <button onClick={clickHandler} value="fiction">
+          Fiction
+        </button>
+        <button onClick={clickHandler} value="business">
+          Business
+        </button>
+        <hr></hr>
+        {list.map((element) => (
+          <div className="box">
+            <h3>{element.book}</h3>
+            <h5>{element.rating}</h5>
+          </div>
+        ))}
+      </nav>
     </div>
   );
 }
